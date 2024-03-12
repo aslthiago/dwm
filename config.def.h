06f03140abbd39e0e7eb44cb2ceb58d2075ff7e1
Thiago Lopes <aslthiago@gmail.com>
2024-03-11 21:08:44 -0300

    Set default font to Fira Code [1] and enable Noto Color Emoji [2]
    [1]: https://archlinux.org/packages/extra/any/ttf-fira-code
    [2]: https://archlinux.org/packages/extra/any/noto-fonts-emoji

--- config.def.h
+++ config.def.h
@@ -5,7 +5,10 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = {
+	"Fira Code:size=10:antialias=true:autohint=true",
+	"Noto Color Emoji:size=8:antialias=true:autohint=true"
+};
 static const char col_black[]       = "#000000";
 static const char col_gray[]        = "#5e5e5e";
 static const char col_gold[]        = "#e59c19";
